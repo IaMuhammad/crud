c0f208fb954f6f46c1c5cb5c4916e8f2396d12d6
muhammad <abduvaliyevmuhammad41@gmail.com>
2024-05-03 09:15:42 +0500

    add admin

--- apps/admin.py
+++ apps/admin.py
@@ -1,3 +1,16 @@
+from email.headerregistry import Group
+
 from django.contrib import admin
 
+from apps.models import Category, Product
+
+
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    ...
